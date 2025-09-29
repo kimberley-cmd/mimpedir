1784a018f7b22884fe6f550c34aeb687f0a3013e
kimberley-cmd <kimberleynascimento29@gmail.com>
2025-09-29 17:26:22 -0300

    criação da tela de editar restaurante

---
+++ lib/tela_editar_restaurante.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mimpedir/tela_home.dart';
+
+class TelaEditarRestaurante extends StatelessWidget{
+  TelaEditarRestaurante({super.key})
+
+  final TextEditingController nome = TextEditingController();
+  final TextEditingController tipo = TextEditingController();
+  final TextEditingController longitude = TextEditingController();
+  final TextEditingController latitude = TextEditingController();
+}
